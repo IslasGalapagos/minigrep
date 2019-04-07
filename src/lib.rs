7eb3e5a6e747a98aa4d5c5cf90627a93471b4b92
Evgeny Sysoletin <evgeny.sysoletin@gmail.com>
2019-04-07 21:06:03 +0300

    add search fn, add tests, ref

--- src/lib.rs
+++ src/lib.rs
@@ -22,7 +22,54 @@ impl Config {
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
 
-    println!("With text:\n{}", contents);
+    for line in search(&config.query, &contents) {
+        println!("{}", line);
+    }
 
     Ok(())
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn creating_config() {
+        let args = vec!["x".to_string(), "y".to_string(), "z".to_string()];
+        let config = Config::new(&args).unwrap();
+
+        assert_eq!(config.query, "y");
+        assert_eq!(config.filename, "z");
+    }
+
+    #[test]
+    #[should_panic]
+    fn creating_config_with_error() {
+        let args = vec!["x".to_string(), "y".to_string()];
+
+        Config::new(&args).unwrap();
+    }
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
 }
